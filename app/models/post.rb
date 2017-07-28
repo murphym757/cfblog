fca80c0bb2986e7ccf43d272d1af8e69221c774c
murphym757 <murphym757@yahoo.com>
2017-07-28 00:13:38 -0400

    Added image upload option and styled around it

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,9 @@
 class Post < ApplicationRecord
   has_many :comments, dependent: :destroy
   validates :title, presence: true, length: {minimum: 5}
-  validates :body, presence: true
+  validates :body, presence: true, length: {maximum: 750}
+
+#For the Paperclip gem
+  has_attached_file :image, styles: { large: "600x600>", medium: "300x300>", thumb: "150x150#" }, default_url: "/assets/cfblog_icon.png"
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
